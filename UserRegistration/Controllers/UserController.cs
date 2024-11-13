c8ec2da1873f64492684ff49ee4ebfd4a8843135
JosseGonnza <accion.quevedo@hotmail.com>
2024-11-13 00:26:59 +0000

    ✨ feat: implement post method and classes User and UserRequest

--- UserRegistration/Controllers/UserController.cs
+++ UserRegistration/Controllers/UserController.cs
@@ -6,5 +6,25 @@ namespace UserRegistration.Controllers;
 [Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
+    [HttpPost]
+    public IActionResult Post(UserRequest request)
+    {
+        var user = new User(request.email, request.password);
+        return CreatedAtAction(nameof(Post), user);
+    }
 }
+
+public class User
+{
+    public Guid Id { get; }
+    public string Email { get; }
+    public string Password { get; }
+
+    public User(string email, string password)
+    {
+        Email = email;
+        Password = password;
+    }
+}
+
+public record UserRequest(string email, string password);
